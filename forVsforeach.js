73969755b624eeeabd3b27636af6da6d98f76cc4
Tresna <52813382+TresnaRani@users.noreply.github.com>
2023-01-31 13:11:22 +0600

    ES6

---
+++ forVsforeach.js
@@ -0,0 +1,14 @@
+
+/*
+var nums = [10,20,30,40];
+for (var x= 0;x<nums.length;x++){
+    console.log(nums[x]);
+}
+*/
+
+
+
+var nums = [50,20,30,40];
+nums.forEach(function(x){
+    console.log(x);
+})

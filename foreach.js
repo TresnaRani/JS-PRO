188026fc42579f90d06235a42bbadc234530a06c
Tresna <52813382+TresnaRani@users.noreply.github.com>
2023-01-31 13:26:27 +0600

    ES^

--- foreach.js
+++ foreach.js
@@ -1,7 +1,18 @@
 
+
+/*
 var nums = [10,20,30,40];
 var sqrtnums = [];
 nums.forEach(function(x){
     sqrtnums.push(x*x);
 })
 console.log(sqrtnums);
+*/
+
+
+var nums = [10,20,30,40];
+console.log(nums);
+nums.forEach(function(x,index,arr){
+    arr[index] = x+5;
+})
+console.log(nums);
